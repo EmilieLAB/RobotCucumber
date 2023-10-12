c4756bf57a1afa270456ee5d1575d27b20c43ef2
EmilieLAB <emilie.labbe@alliance4u.fr>
2023-10-12 16:37:21 +0200

    CartographieFixture completion

--- src/test/java/robot/CartographieFixture.java
+++ src/test/java/robot/CartographieFixture.java
@@ -2,6 +2,10 @@ package robot;
 
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.Then;
+import org.junit.Assert;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class CartographieFixture {
     private MyWorld world;
@@ -17,7 +21,18 @@ public class CartographieFixture {
 
     @Then("^la carte du robot mesure (\\d+) x (\\d+) cases$")
     public void laCarteMesure(int sizeX, int sizeY) throws Throwable {
-        // TODO
+        Assert.assertEquals(sizeX, localCountOccurrences(world.getRobot().carte().get(3)) - 3);
+        Assert.assertEquals(sizeY, world.getRobot().carte().size() - 3);
+    }
+
+    private static int localCountOccurrences(String str) {
+        Matcher matcher = Pattern.compile(String.valueOf('\t'))
+                .matcher(str);
+        int counter = 0;
+        while (matcher.find()) {
+            counter++;
+        }
+        return counter;
     }
 
 }
